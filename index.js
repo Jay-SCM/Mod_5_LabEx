1ceba61d903e66ce6df07bf0019bd8d2db6f3e2c
Jay-SCM <John.Farmer1086@gmail.com>
2023-11-11 02:30:37 +0930

    finished swag

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-
-
-
-
-
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
 const express = require('express');
 const CalcRoutes = require('./routes/CalcRoutes');
 const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 swaggerDocument = require('./swagger.json');
-
 const app = express();
 const port = 3000;
-
 // Serve static files from the 'public' directory
 app.use('/', express.static(path.join(__dirname, 'public')));
-
-// Map the calculator routes to our app with the '/calculator' prefix
+// Map the calculator routes
 app.use('/calculator', CalcRoutes);
-
 // Handle requests to the root path by sending the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));});
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`);});
 
