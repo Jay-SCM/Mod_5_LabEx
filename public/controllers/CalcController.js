2770c51803bdc95e98017e3d5fb7b636b594c61a
Jay-SCM <John.Farmer1086@gmail.com>
2023-11-11 09:47:37 +0930

    finished swag

--- public/controllers/CalcController.js
+++ public/controllers/CalcController.js
@@ -13,30 +13,23 @@ const multiplyNumbers = (req, res) => {
     let product = number1 * number2
     console.log(product)
     res.status(200)
-    res.json({result:product})
-}
-
+    res.json({result:product})}
 const subtractNumbers = (req, res) => {
     let number1 = parseInt(req.query.num1);
     let number2 = parseInt(req.query.num2);
     let difference = number1 - number2
     console.log(difference)
     res.status(200)
-    res.json({result:difference})
-}
-
+    res.json({result:difference})}
 const divideNumbers = (req, res) => {
     let number1 = parseInt(req.query.num1);
     let number2 = parseInt(req.query.num2);
     let fraction = number1 / number2
     console.log(fraction)
     res.status(200)
-    res.json({result:fraction})
-}
-
+    res.json({result:fraction})}
 module.exports = {
-    addNumbers, multiplyNumbers, subtractNumbers, divideNumbers
-}
+    addNumbers, multiplyNumbers, subtractNumbers, divideNumbers}
 
 // http://localhost:3000/controllers/CalcController.js:37
 
